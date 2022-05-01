91e93a3a12c6f221399d80c89de04e31727f02d7
ToshiroR <97269007+ToshiroR@users.noreply.github.com>
2022-05-01 19:52:03 -0400

    first part of playRound completed

--- script.js
+++ script.js
@@ -8,23 +8,35 @@
 // function that returns rock paper scissors!
 
 function computerPlay() {
-    return Math.floor(Math.random() * 3)
-} if (computerPlay() === 1){
-    console.log("rock");
-} else if (computerPlay() === 2) {
-    console.log("paper");
+   let play = Math.floor(Math.random() * 3)
+ if (play === 1){
+    return "rock";
+} else if (play === 2) {
+    return "paper";
 } else {
-    console.log("scissors");
+    return "scissors";
+}
 };
 
 
 const computer = computerPlay();
 const player = "rock"; 
 
-
-computerPlay()
+console.log(computer)
+console.log(player)
 
 // function that takes the player and computers input, compares the 2 and determines the winner
 
 
 //function playRound(player, computer)
+
+function playRound(player, computer) {
+    if (player === computer) {
+        console.log("Tie!")
+    } else if (player === "rock" && computer === "scissors") {
+        console.log("wow")
+    }
+}
+
+
+playRound(player, computer)
