1180bd5d402a5105351ee5180fdf908064686ff1
ToshiroR <97269007+ToshiroR@users.noreply.github.com>
2022-05-01 18:25:19 -0400

    completed function for computerPlay

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+
+
+
+
+
+
+// function that returns rock paper scissors!
+
+function computerPlay() {
+    return Math.floor(Math.random() * 3)
+} if (computerPlay() === 1){
+    console.log("rock");
+} else if (computerPlay() === 2) {
+    console.log("paper");
+} else {
+    console.log("scissors");
+};
+
+
+const computer = computerPlay();
+const player = "rock"; 
+
+
+computerPlay()
+
+// function that takes the player and computers input, compares the 2 and determines the winner
+
+
+//function playRound(player, computer)
